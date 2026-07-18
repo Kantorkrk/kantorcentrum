e541d0b422128f9e0e47ede0d62ca06493829594
Kantorkrk <fotopasja-88@wp.pl>
2026-07-18 14:17:25 +0200

    Add currency data for EUR, USD, GBP, and CHF

--- kursy.js
+++ kursy.js
@@ -1,1 +1,22 @@
-
+const currencies = [
+    {
+        code: "EUR",
+        buy: 4.24,
+        sell: 4.28
+    },
+    {
+        code: "USD",
+        buy: 3.63,
+        sell: 3.68
+    },
+    {
+        code: "GBP",
+        buy: 4.92,
+        sell: 4.99
+    },
+    {
+        code: "CHF",
+        buy: 4.50,
+        sell: 4.57
+    }
+];
