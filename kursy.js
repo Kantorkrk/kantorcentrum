15c047af61bf06b269b16f8cc9d8c49794c2745a
Kantorkrk <fotopasja-88@wp.pl>
2026-07-19 13:54:23 +0200

    Update kursy.js

--- kursy.js
+++ kursy.js
@@ -1,22 +1,74 @@
 const currencies = [
     {
         code: "EUR",
+        flag: "🇪🇺",
         buy: 4.26,
         sell: 4.35
     },
     {
         code: "USD",
+        flag: "🇺🇸",
         buy: 3.71,
         sell: 3.80
     },
     {
         code: "GBP",
+        flag: "🇬🇧",
         buy: 4.94,
         sell: 5.10
     },
     {
         code: "CHF",
+        flag: "🇨🇭",
         buy: 4.50,
         sell: 4.57
+    },
+    {
+        code: "AUD",
+        flag: "🇦🇺",
+        buy: 2.57,
+        sell: 2.65
+    },
+    {
+        code: "CAD",
+        flag: "🇨🇦",
+        buy: 2.65,
+        sell: 2.72
+    },
+    {
+        code: "CZK",
+        flag: "🇨🇿",
+        buy: 0.16,
+        sell: 0.18
+    },
+    {
+        code: "DKK",
+        flag: "🇩🇰",
+        buy: 0.56,
+        sell: 0.59
+    },
+    {
+        code: "NOK",
+        flag: "🇳🇴",
+        buy: 0.36,
+        sell: 0.39
+    },
+    {
+        code: "SEK",
+        flag: "🇸🇪",
+        buy: 0.37,
+        sell: 0.40
+    },
+    {
+        code: "HUF",
+        flag: "🇭🇺",
+        buy: 0.010,
+        sell: 0.012
+    },
+    {
+        code: "JPY",
+        flag: "🇯🇵",
+        buy: 0.023,
+        sell: 0.026
     }
 ];
