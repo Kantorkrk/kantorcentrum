7fd938df65a3cb422d1ee4b02f00774970be4249
Kantorkrk <fotopasja-88@wp.pl>
2026-08-11 09:14:12 +0200

    Update kursy.js

--- kursy.js
+++ kursy.js
@@ -22,7 +22,7 @@ const currencies = [
         code: "USD old",
         flag: "🇺🇸",
         buy: 3.67,
-        sell: 3.724
+        sell: 3.726
     },
     {
         code: "GBP Funt brytyjski",
