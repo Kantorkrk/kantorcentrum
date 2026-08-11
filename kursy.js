25ced0bc3b826e766a84b2cf94c66a8531959424
Kantorkrk <fotopasja-88@wp.pl>
2026-08-11 09:13:51 +0200

    Update kursy.js

--- kursy.js
+++ kursy.js
@@ -3,13 +3,13 @@ const currencies = [
         code: "EUR EURO",
         flag: "🇪🇺",
         buy: 4.26, 
-        sell: 4.318
+        sell: 4.317
     },
      {
         code: "EUR >10k EURO",
         flag: "🇪🇺",
         buy: 4.287,
-        sell: 4.313
+        sell: 4.309
 
     },
     {
