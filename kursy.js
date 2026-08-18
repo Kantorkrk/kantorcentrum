ec7a1d59d484f06ce96756743a97252ef74b43dd
Kantorkrk <fotopasja-88@wp.pl>
2026-08-18 12:11:16 +0200

    Update kursy.js

--- kursy.js
+++ kursy.js
@@ -16,7 +16,7 @@ const currencies = [
         code: "USD Dolar od 2009",
         flag: "🇺🇸",
         buy: 3.714,
-        sell: 3.741
+        sell: 3.742
     },
      {
         code: "USD old",
